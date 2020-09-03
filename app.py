b741de481beccfe12d17839fd589280f37387215
Krzysztof Babij <keybi81@gmail.com>
2020-09-03 13:19:56 +0200

    conditionals

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, url_for, redirect, session
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template
 import requests
 
 
@@ -16,7 +16,7 @@ def hello_world():
 @app.route('/home/<string:name>', methods=['POST', 'GET'])
 def home(name):
     session['name'] = name
-    return f'<h1>Hello {name}, you are on the homepage!</h1>'
+    return render_template('home.html', name=name)
 
 
 @app.route('/json')
@@ -38,11 +38,7 @@ def query():
 @app.route('/theform', methods=['GET', 'POST'])
 def theform():
     if request.method == 'GET':
-        return '''<form method="POST" action="/theform">
-                    <input type="text" name="name">
-                    <input type="text" name="location">
-                    <input type="submit" value="Wyślij">
-                  </form>'''
+        return render_template('form.html')
     else:
         name = request.form['name']
         location = request.form['location']
